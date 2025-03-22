11801448397a181710617928146235cb6f0bb4ba
SantiagoRB17 <santiago.ramirezb1@uqvirtual.edu.co>
2025-03-22 16:54:45 -0500

    Actualizacion del module.info
    +2 dependencias necesarias agregadas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module co.edu.uniquindio.poo.gestor_de_contactos {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires static lombok;
+    requires com.dlsc.gemsfx;
+    requires jfxtras.controls;
 
     opens co.edu.uniquindio.poo.gestor_de_contactos to javafx.fxml;
     exports co.edu.uniquindio.poo.gestor_de_contactos;
